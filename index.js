da2b7b8b2dfc6424c37c776e8454bd97d327a643
Angelene Manalo <angelenelm@gmail.com>
2022-03-16 10:31:24 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import "dotenv/config";
+import express from "express";
+import axios from "axios";
+const app = express();
+const port = 8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+console.log(process.env.CLIENT_ID);
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = "spotify_auth_state";
+
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = "user-read-private user-read-email";
+
+  const params = new URLSearchParams({
+    client_id: CLIENT_ID,
+    response_type: "code",
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${params}`);
+});
+
+app.get("/callback", (req, res) => {
+  const code = req.query.code ?? null;
+
+  axios({
+    method: "post",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening at port ${port}`);
+});
